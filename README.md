c4ba6e1e05acd39e6eb745ba6a353ea2a4e558e0
renjie <renjie_dlut2016@163.com>
2022-03-30 11:50:21 +0800

    在学习路线中添加：多项式参考资料

--- README.md
+++ README.md
@@ -16,14 +16,49 @@
    [参考代码](https://github.com/ApolloAuto/apollo/blob/master/modules/planning/common/path/frenet_frame_path.cpp)
 
 2. 5次多项式，3次样条，5次样条曲线：
+
+   理解插值与拟合的区别：
+
+   lattice规划算法：横向规划是5次多项式，纵向规划是（4次或则5次）；
+
+   DP路径规划：采用的是3次样条曲线；
+
+   曲线光滑：    螺旋线，2维的5次样条曲线；
+
+   QP路径规划：piecewise_jerk_path(分段jerk路径)，本质就是3次样条曲线
+
+   QP速度规划：piecewise_jerk_speed(分段jerk速度)，本质是3次样条曲线
+
+   [三次多项式](https://github.com/ApolloAuto/apollo/blob/master/modules/planning/math/curve1d/cubic_polynomial_curve1d.h)
+
+   [四次多项式](https://github.com/ApolloAuto/apollo/blob/master/modules/planning/math/curve1d/quartic_polynomial_curve1d.h)
+
+   [五次多项式](https://github.com/ApolloAuto/apollo/blob/master/modules/planning/math/curve1d/quintic_polynomial_curve1d.h)
+
+   [三次样条插值(知乎)](https://zhuanlan.zhihu.com/p/62860859)
+
+   [piecewise_jerk(分段加加速度曲线，本质就是三次样条曲线)](https://github.com/ApolloAuto/apollo/blob/master/modules/planning/common/trajectory1d/piecewise_trajectory1d.h)
+
+   
+
 3. 静态障碍物映射到SL 图中：
+
 4. 动态障碍物映射到ST图中：
+
 5. lattice规划方法：
+
 6. 什么是优化问题：
+
 7. 构建参考线平滑优化问题：
+
 8. 构建路径规划优化问题：
+
 9. 熟悉A*算法：
+
 10. 什么是动态规划问题：
+
 11. 采用动态规划解决速度规划问题：
+
 12. 构建速度规划优化问题：
+
 13. 熟悉dubins、reed-shepp曲线：
--- auto_drive/planning/frenet_frame/cartesian_frenet_conversion.cpp
+++ auto_drive/planning/frenet_frame/cartesian_frenet_conversion.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 void CartesianFrenetConverter::cartesian_to_frent(const double rs, const double rx, const double ry, const double rtheta, const double rkappa, const double rdkappa,
-	const double x, const double y, const double v, const double a, const double theta, const double kappa)
+	const double x, const double y, const double v, const double a, const double theta, const double kappa,std::array<double, 3>* const ptr_s_condition,std::array<double, 3>* const ptr_d_condition)
 {
 	 double s_condition[3] = {0 ,0, 0};
 	 double d_condition[3] = {0 ,0, 0};
 
+    ptr_s_condition->at(0)=rs;
 
 	const double dx = x - rx;
 	const double dy = y - ry;
@@ -19,6 +20,7 @@ void CartesianFrenetConverter::cartesian_to_frent(const double rs, const double 
 	const double cross_rd_nd = cos_theta_r * dy - sin_theta_r * dx;
 
 	 d_condition[0]= copysign(std::sqrt(dx * dx + dy * dy), cross_rd_nd);
+	 ptr_d_condition->at(0)=d_condition[0];
 
 	const double delta_theta = theta - rtheta;
 	const double tan_delta_theta = tan(delta_theta);
@@ -26,57 +28,50 @@ void CartesianFrenetConverter::cartesian_to_frent(const double rs, const double 
 
 	const double one_minus_kappa_r_d = 1 - rkappa * d_condition[0];
 	d_condition[1] = one_minus_kappa_r_d * tan_delta_theta;
+	ptr_d_condition->at(1)=d_condition[1];
 	const double kappa_r_d_prime = rdkappa * d_condition[0] + rkappa * d_condition[1];
 
 	d_condition[2] = (-kappa_r_d_prime * tan_delta_theta +one_minus_kappa_r_d / cos_delta_theta / cos_delta_theta *(kappa * one_minus_kappa_r_d / cos_delta_theta - rkappa));
-	s_condition[1] = rs;
+    ptr_d_condition->at(2)=d_condition[2];
 	s_condition[1] = v * cos_delta_theta / one_minus_kappa_r_d;
+	ptr_s_condition->at(1)=s_condition[1];
 	const double delta_theta_prime = one_minus_kappa_r_d / cos_delta_theta * kappa - rkappa;
 	s_condition[2] = ((a * cos_delta_theta -s_condition[1] * s_condition[1] *(d_condition[1] * delta_theta_prime - kappa_r_d_prime))/one_minus_kappa_r_d);
-	//return s_condition[], d_condition[];  //C++不能返回数组，需要定义一个指向数组的指针，返回指针的地址
+	ptr_s_condition->at(2)=s_condition[2];
+    //return s_condition[], d_condition[];  //C++不能返回数组，需要定义一个指向数组的指针，返回指针的地址
 }
 
 void  CartesianFrenetConverter::frent_to_cartesian(const double rs, const double rx, const double ry, const double rtheta, const double rkappa, const double rdkappa,
-												const double s_condition[3], const double d_condition[3])
+												const double s_condition[3], const double d_condition[3],double &x, double &y,double &theta,double &kappa,double &v,double& a)
 {
 	if (fabs(rs - s_condition[0]) > 0.001)
 	{
 		printf("The reference point s and s_condition[0] don't match");
 	}
 
-		
 	const double	cos_theta_r = cos(rtheta);
 	const double    sin_theta_r = sin(rtheta);
 
-	const double x = rx - sin_theta_r * d_condition[0];
-	const double y = ry + cos_theta_r * d_condition[0];
+	 x = rx - sin_theta_r * d_condition[0];
+	 y = ry + cos_theta_r * d_condition[0];
 
 	const double one_minus_kappa_r_d = 1 - rkappa * d_condition[0];
 	const double tan_delta_theta = d_condition[1] / one_minus_kappa_r_d;
 	const double delta_theta = atan2(d_condition[1], one_minus_kappa_r_d);
 	const double cos_delta_theta = cos(delta_theta);
 
-	const double theta = 0;//NormalizeAngle(delta_theta + rtheta);  //C++归一化函数？
+	theta = 0;//NormalizeAngle(delta_theta + rtheta);  //C++归一化函数？
 	const double kappa_r_d_prime = rdkappa * d_condition[0] + rkappa * d_condition[1];
 
-	const double kappa = ((((d_condition[2] + kappa_r_d_prime * tan_delta_theta) *
+	 kappa = ((((d_condition[2] + kappa_r_d_prime * tan_delta_theta) *
 		cos_delta_theta * cos_delta_theta) /(one_minus_kappa_r_d)+rkappa) * cos_delta_theta / (one_minus_kappa_r_d));
 
 	const double d_dot = d_condition[1] * s_condition[1];
 
-	const double v = sqrt(one_minus_kappa_r_d * one_minus_kappa_r_d * s_condition[1] * s_condition[1] + d_dot * d_dot);
+	v = sqrt(one_minus_kappa_r_d * one_minus_kappa_r_d * s_condition[1] * s_condition[1] + d_dot * d_dot);
 	const double delta_theta_prime = one_minus_kappa_r_d / cos_delta_theta * (kappa)-rkappa;
 
-	const double a = (s_condition[2] * one_minus_kappa_r_d / cos_delta_theta +
+	a = (s_condition[2] * one_minus_kappa_r_d / cos_delta_theta +
 		s_condition[1] * s_condition[1] / cos_delta_theta *
 		(d_condition[1] * delta_theta_prime - kappa_r_d_prime));
-	//return x, y, v, a, theta, kappa;
-}
-
-
-int main()
-{
-	
-	
-	return 0;
 }
