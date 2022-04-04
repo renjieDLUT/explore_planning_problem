218cbe5b48339e64e9c9382f8b3fa687180ba7a8
182yyj <33393729+182yyj@users.noreply.github.com>
2022-04-04 11:32:20 +0800

    多项式拟合
    多项式拟合，Apollo代码：
    1、基类：曲线拟合
    2、派生类：一维曲线拟合

---
+++ auto_drive/planning/curve1d/curve1d.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<string>
+
+class Curve1d {
+public:
+    Curve1d() = default;
+
+    virtual ~Curve1d() = default;
+
+    virtual double Evaluate(const std::uint32_t order,
+        const double param) const = 0;
+
+    virtual double ParamLength() const = 0;
+
+    virtual std::string ToString() const = 0;
+};
---
+++ auto_drive/planning/curve1d/main.cpp
@@ -0,0 +1,8 @@
+#include"curve1d.h"
+#include"polynomial_curve1d.h"
+#include"quintic_polynomial_curve1d.h"
+
+int  main()
+{
+	return 0;
+}
