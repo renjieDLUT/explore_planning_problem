3ac993bba5737b3e5f3e66dedc9c7886de4c9123
renjie <renjie&qingtiantruck.com>
2022-06-30 11:22:54 +0800

    add map_test file

--- advanced/my_array/my_array_test.cpp
+++ advanced/my_array/my_array_test.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "array.h"
 #include "gtest/gtest.h"
 TEST(MYARRAY, Ctor) {
@@ -6,4 +8,11 @@ TEST(MYARRAY, Ctor) {
   EXPECT_EQ(vec.size(), 4);
   vec[1] = 2;
   EXPECT_EQ(vec[1], 2);
+}
+
+TEST(MYARRAY, IndexOpt) {
+  const Array<int> vec;
+  int a = int();
+
+  std::cout << vec[3] << std::endl << a << std::endl;
 }
